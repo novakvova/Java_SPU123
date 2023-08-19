3d6cfcbecbcab3b02480b92718aa950d04d8077d
novakvova <novakvova@gmail>
2023-08-19 11:48:46 +0300

    Working app is good

---
+++ _2.WebApp/src/main/java/org/example/entities/CategoryEntity.java
@@ -0,0 +1,17 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="tbl_categories")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="name", length = 250, nullable = false)
+    private String name;
+    @Column(name="description", length = 4000, nullable = true)
+    private String description;
+}
