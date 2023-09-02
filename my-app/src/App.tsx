b5234a4a6120d7de7149c455c8cad158806d8632
novakvova <novakvova@gmail>
2023-09-02 13:05:52 +0300

    Bwgin working create and delete category

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import DefaultLayout from "./components/containers/default/DefaultLayout.tsx";
 import CategoryListPage from "./components/admin/category/list/CategoryListPage.tsx";
+import CategoryCreatePage from "./components/admin/category/create/CategoryCreatePage.tsx";
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
             <Route path={"/"} element={<DefaultLayout/>}>
                 <Route index element={<CategoryListPage/>}/>
+                <Route path={"create"} element={<CategoryCreatePage/>}/>
             </Route>
         </Routes>
     </>
